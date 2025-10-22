a3a3aa28654c0e37f68560d0de98a1b77956fb2b
juan manuel <juanmanuelgouzou@gmail.com>
2025-10-21 21:30:38 -0300

    csv

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const { SerialPort } = require('serialport');
 // const { SerialPort } = require('@serialport/stream');
 const bindings  = require('@serialport/bindings-cpp');
 SerialPort.bindings = bindings;
-const { stringify } = require('csv-stringify');
 
+const fs = require('fs');
+const { Parser } = require('json2csv');
 const express = require('express')
 const app = express()
 app.use(express.static('views'));
@@ -20,9 +21,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/resultado', (req, res) => {
+  
   resultado = req.body;
-  console.log(resultado);
+  // console.log(resultado);
+  function archivoExiste(filePath) {
+    if (fs.existsSync(filePath)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const archivoCSV = "./streamdeck.csv";
+  if(!archivoExiste(archivoCSV)){
+    const datos = [
+      ['boton', 'direccion'],
+      [resultado['boton'], resultado['archivo']],
+    ];
+    const csvContentenido = datos.map(row => row.join(',')).join('\n');
+
+    fs.writeFile('streamdeck.csv', csvContentenido, (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log('CSV file "output.csv" has been saved.');
+      }
+    });
+  }else{
+    const nuevosDatos = resultado['boton']+","+resultado['archivo']+'\n';
+    fs.appendFile(archivoCSV, nuevosDatos, (err) => {
+        if (err) {
+            console.error('Error appending data to CSV:', err);
+            return;
+        }
+        console.log('Data successfully appended to CSV.');
+    });
+  }
+
+  fs.readFile('streamdeck.csv', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    // Process the CSV data here
+    const filas = data.split('\n');
+    const encabezados = filas[0].split(',');
+    const arrayDatos = [];
+
+    for (let i = 1; i < filas.length; i++) {
+        const valores = filas[i].split(',');
+        const entry = {};
+        for (let j = 0; j < encabezados.length; j++) {
+            entry[encabezados[j]] = valores[j];
+        }
+        arrayDatos.push(entry);
+    }
+    console.log(arrayDatos);
+});
+
+  res.redirect('/');
 })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
@@ -36,7 +94,6 @@ puerto.on('open', () => {
     console.log('Serial Port Open');
 });
 let resultado = null;
-console.log(resultado);
 puerto.on('data', (data) => {
     const keypressed = data.toString().trim(); // Convert buffer to string and remove whitespace
     // console.log('Key Pressed:', keypressed);
@@ -68,19 +125,19 @@ puerto.on('data', (data) => {
           // console.log(`Salida del comando: ${stdout}`);
           });
         break;
-      // case "A":
-      //   exec('start code', (error, stdout, stderr) => {
-      //     if (error) {
-      //       console.error(`Error al ejecutar el comando: ${error.message}`);
-      //       return;
-      //     }
-      //     if (stderr) {
-      //       console.error(`Salida de error: ${stderr}`);
-      //       return;
-      //     }
-      //     // console.log(`Salida del comando: ${stdout}`);
-      //     });
-      //   break;
+      case "A":
+        exec('start code', (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error al ejecutar el comando: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.error(`Salida de error: ${stderr}`);
+            return;
+          }
+          // console.log(`Salida del comando: ${stdout}`);
+          });
+        break;
       // case "9":
       //   exec('start https://www.instagram.com', (error, stdout, stderr) => {
       //     if (error) {
